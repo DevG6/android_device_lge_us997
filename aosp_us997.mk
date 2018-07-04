3989cb7124f0aa8a7583612aee03a7343a75fff8
Rashed Abdel-Tawab <rashed@linux.com>
2018-07-04 09:21:15 -0700

    us997: Set device and product names to lucye
    Change-Id: I0b0e9f13a7db0f3e4af8bfa2413d128458ec7d1b
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- aosp_us997.mk
+++ aosp_us997.mk
@@ -11,7 +11,8 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 PRODUCT_NAME := aosp_us997
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE="g6" \
+    TARGET_DEVICE="lucye" \
+    PRODUCT_DEVICE="lucye" \
     PRODUCT_NAME="lucye_nao_us" \
     PRIVATE_BUILD_DESC="lucye_nao_us-user 7.0 NRD90U 172061549d102 release-keys"
 
